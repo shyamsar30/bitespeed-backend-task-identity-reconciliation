be58066ab607682e69c8f3c9aa229ae9a73721ce
shyamsar30 <shyamsartanpara2019@gecg28.ac.in>
2023-06-18 01:32:51 +0530

    Database Model created.

---
+++ backend/database/models.py
@@ -0,0 +1,22 @@
+from datetime import datetime
+from sqlalchemy import Column, Enum, Integer, String, DateTime
+
+from backend.database.datatypes import LinkPrecedenceTypes
+from connector import Base, db_engine
+
+
+class User(Base):
+    __tablename__ = "users"
+
+    id = Column(Integer, primary_key=True, index=True, autoincrement='auto')
+    phoneNumber = Column(String)
+    email = Column(String)
+    linkedId = Column(Integer)
+    linkPrecedence = Column(Enum(*LinkPrecedenceTypes.all(), name="link_precedence_types"), nullable=False)
+    createdAt = Column(DateTime, nullable=False, default=datetime.utcnow)
+    updatedAt = Column(DateTime, nullable=False, default=datetime.utcnow, onupdate=datetime.utcnow)
+    deletedAt = Column(DateTime)
+
+# Create all the tables in database
+if __name__ == "__main__":
+    Base.metadata.create_all(db_engine)
