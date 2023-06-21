ef11d790be8974c30084441a8c0a2ae47d8a0acd
shyamsar30 <shyamsartanpara2019@gecg28.ac.in>
2023-06-21 13:58:21 +0530

    Endpoint Typo Corrected.

--- README.md
+++ README.md
@@ -2,5 +2,5 @@
 
 This is a Flask application. It is hosted at https://bitespeed-backend-task-identity-f4ta.onrender.com with Swagger UI. \
 The database used for this app is PosgreSQL. \
-The **/identify** endpoint is available at https://bitespeed-backend-task-identity-f4ta.onrender.com/api/identity. \
+The **/identify** endpoint is available at https://bitespeed-backend-task-identity-f4ta.onrender.com/api/identify. \
 My Resume is also uploaded in the root directory: https://github.com/shyamsar30/bitespeed-backend-task-identity-reconciliation/blob/main/Resume_Shyam.pdf
--- backend/api/endpoints.py
+++ backend/api/endpoints.py
@@ -7,10 +7,10 @@ from backend.api.views import identify
 
 namespace = Namespace(name="Identity Namespace", path='/')
 
-@namespace.route('/identity')
+@namespace.route('/identify')
 class IdentityView(Resource):
     POST_DOC_MODEL = namespace.model(
-        "Identity",
+        "Identify",
         {
             "email": fields.String(example="shyamsar30@gmail.com", description="Email Address"),
             "phoneNumber": fields.String(example="1234567898", description="Mobile Number")
@@ -20,7 +20,7 @@ class IdentityView(Resource):
     @namespace.expect(POST_DOC_MODEL)
     def post(self):
         """
-        Identity Endpoint
+        Identify Endpoint
         """
         validated_json = UserValidator().load(request.json)
         return identify(validated_json)
